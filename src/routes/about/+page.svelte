9bbeb9731db6641ca67a67d1673b636160415abf
Spimy <officialspyme@gmail.com>
2023-04-05 20:55:23 +0400

    Add content to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,28 @@
-<h1>About Page</h1>
+<section>
+	<h2>Purpose</h2>
+	<p>
+		I created this because I was bored 😴 but I didn't have time to start a new, larger project
+		since at the time of creating this, I had a day left before I have a flight for vacation.
+		<br />
+		<br />
+		At the same time, I wanted to give SvelteKit. Building a weather app is one of the many beginner
+		projects so I thought, why not? That's how this page was created. Nothing fancy ✨.
+	</p>
+</section>
+<section>
+	<h2>Attributions</h2>
+	<p>
+		This web application was made possible thanks to<a
+			rel="external"
+			href="https://openweathermap.org/"
+			target="_blank">OpenWeather's</a
+		>amazing API. Without it, there would be no real way to get access to weather data. If you are
+		creating your own weather app, I recommend checking them out 😃.
+	</p>
+</section>
+
+<style lang="scss">
+	section {
+		margin: 2em 0;
+	}
+</style>
