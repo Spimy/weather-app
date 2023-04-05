98b8f174c7b924813bcc162667c9b9a28136cee9
Spimy <officialspyme@gmail.com>
2023-04-05 20:27:42 +0400

    Add footer to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import Navbar from '$lib/navbar.svelte';
 	import '../app.scss';
+
+	const currentYear = new Date().getFullYear();
 </script>
 
 <main>
 	<Navbar />
 	<slot />
+
+	<footer>
+		<p>Copyright &#169; {currentYear} Spimy</p>
+	</footer>
 </main>
+
+<style lang="scss">
+	footer {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		left: 0;
+		text-align: center;
+		padding: 1em;
+	}
+</style>
