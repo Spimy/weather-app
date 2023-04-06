2c7ffeeef7446dbc5647bf2b76a7f04571832048
Spimy <officialspyme@gmail.com>
2023-04-06 15:45:23 +0400

    Add attribution for geodb cities api

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
 		since at the time of creating this, I had a day left before I have a flight for vacation.
 		<br />
 		<br />
-		At the same time, I wanted to give SvelteKit. Building a weather app is one of the many beginner
+		At the same time, I wanted to give SvelteKit a try. Building a weather app is one of the many beginner
 		projects so I thought, why not? That's how this page was created. Nothing fancy ✨.
 	</p>
 </section>
@@ -18,6 +18,12 @@
 			target="_blank">OpenWeather's</a
 		>amazing API. Without it, there would be no real way to get access to weather data. If you are
 		creating your own weather app, I recommend checking them out 😃.
+		<br />
+		<br />
+		I would like to also extend my thanks to
+		<a rel="external" href="https://rapidapi.com/wirefreethought/api/geodb-cities" target="_blank"
+			>GeoDB Cities'</a
+		> for its API which allowed me to create the autocomplete functionality of the search bar 👍.
 	</p>
 </section>
 
