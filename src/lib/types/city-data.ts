29db2937ab3a3c7d50950d76b470639f31e0edc2
Spimy <officialspyme@gmail.com>
2023-04-06 15:14:20 +0400

    Add city data interfaces

---
+++ src/lib/types/city-data.ts
@@ -0,0 +1,31 @@
+export interface CityData {
+	data: Datum[];
+	links: Link[];
+	metadata: Metadata;
+}
+
+export interface Datum {
+	id: number;
+	wikiDataId: string;
+	type: string;
+	city: string;
+	name: string;
+	country: string;
+	countryCode: string;
+	region: string;
+	regionCode: string;
+	regionWdId: string;
+	latitude: number;
+	longitude: number;
+	population: number;
+}
+
+export interface Link {
+	rel: string;
+	href: string;
+}
+
+export interface Metadata {
+	currentOffset: number;
+	totalCount: number;
+}
