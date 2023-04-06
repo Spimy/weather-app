662e460aa22deb8cfbd64a29250065f36479dc4d
Spimy <officialspyme@gmail.com>
2023-04-06 16:18:58 +0400

    Add contact info page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,21 @@
+<section>
+	<h2>Contact</h2>
+	<p>
+		Hello there! If you need to contact me, please use the contact form on my <a
+			rel="external"
+			href="https://spimy.dev/contact"
+			target="_blank">portfolio's contact page.</a
+		>
+		If it does not working then please reach out to me via my social media links which are in the footer
+		on the same page.
+
+		<br />
+		<br />
+
+		If you are trying to reach me to report a bug or to add new features to this web application,
+		then please, instead of filling in the contact form in the aforementioned page, open a
+		<a rel="external" href="https://github.com/Spimy/weather-app/issues" target="_blank"
+			>GitHub issue</a
+		> and I will take a look at it whenever I have time.
+	</p>
+</section>
